80f4d670ace448b2fe9dbbfbf8213655200d8848
Buriak-byte <oleksii.buriak12@gmail.com>
2026-06-12 19:09:07 +0300

    dddd

---
+++ src/js/renderGames.js
@@ -0,0 +1,8 @@
+import { games } from "./data/games.js";
+const gamesContainer = document.querySelector('[data-game-container]');
+function renderGames(gamesData, container){
+if(!container){
+console.error("контейнер незнайдено")
+return
+}
+}
