fbd333d31111c0e19e7cc478243407e841748e27
roman3857 <romanshakhrai6@gmail.com>
2026-07-01 16:23:49 +0200

    commit

---
+++ src/js/games/dino.js
@@ -0,0 +1,7 @@
+export class dinoGame {
+    constructor(dino, gameArea) {
+        this.dino = dino
+        this.area = gameArea
+        
+    }
+}
--- src/js/games/dinoGame.js
+++ src/js/games/dinoGame.js
@@ -1,7 +1,15 @@
+import { dinoGame } from './dino';
 export function initDinoGmae() {
-    const gameContainer = document.querySelector('#game-6')
-    console.log(gameContainer)
-    const markup = `
-    <div>
-    `
+  const gameContainer = document.querySelector('#game-6');
+  console.log(gameContainer);
+  const markup = `
+          <h2 class="dinoGame__title">Google динозавр</h2>
+         <div id="dinoGameArea" class="dinoGame__box">
+            <div id="dino" class="dinoGame__character">
+            </div>
+    `;
+  gameContainer.innerHTML = markup;
+  const gameArea = document.querySelector('#dinoGameArea');
+  const dino = document.querySelector('#dino');
+  new dinoGame(dino, gameArea);
 }
