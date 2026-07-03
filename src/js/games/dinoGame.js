6062786e9cbe81ad9d82d69686277698eab89ae6
roman3857 <romanshakhrai6@gmail.com>
2026-07-03 17:30:03 +0200

    update dino

--- src/js/games/dinoGame.js
+++ src/js/games/dinoGame.js
@@ -1,11 +1,11 @@
 import { dinoGame } from './dino';
 export function initDinoGmae() {
   const gameContainer = document.querySelector('#game-6');
-  console.log(gameContainer);
   const markup = `
           <h2 class="dinoGame__title">Google динозавр</h2>
          <div id="dinoGameArea" class="dinoGame__box">
             <div id="dino" class="dinoGame__character">
+            </div>
             </div>
     `;
   gameContainer.innerHTML = markup;
